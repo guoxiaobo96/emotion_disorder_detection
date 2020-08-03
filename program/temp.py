19fb5683b44e4f7209add135134159f898bdf94f
guoxiaobo96 <guoxiaobo96@gmail.com>
2020-08-03 11:37:14 -0400

    update a little

--- program/temp.py
+++ program/temp.py
@@ -3,58 +3,33 @@ import shutil
 import zstandard as zstd
 import json
 import lzma
+user_file_list = './data/full_user_list/depression_user_list'
+data_path_list = './data/full_reddit/depression'
+suffix_list = ['.before', '.after']
 
-class Zreader:
+max_seq = 142
+user_set = set()
+with open(user_file_list,mode='r',encoding='utf8') as fp:
+    for line in fp.readlines():
+            user_set.add(line.split(' [info] ')[0])
 
-    def __init__(self, file, chunk_size=16384):
-        '''Init method'''
-        self.fh = open(file,'rb')
-        self.chunk_size = chunk_size
-        self.dctx = zstd.ZstdDecompressor()
-        self.reader = self.dctx.stream_reader(self.fh)
-        self.buffer = ''
-
-
-    def readlines(self):
-        '''Generator method that creates an iterator for each line of JSON'''
-        while True:
-            chunk = self.reader.read(self.chunk_size).decode()
-            if not chunk:
-                break
-            lines = (self.buffer + chunk).split("\n")
-
-            for line in lines[:-1]:
-                yield line
-
-            self.buffer = lines[-1]
-
-    def close(self):
-        self.fh.close()
-
-reader = Zreader('F:/reddit/submissions/RS_2019-02.zst', chunk_size=8192)
-# reader = lzma.open('F:/reddit/submissions/RS_2017-07.xz')
+user_count = 0
 count = 0
-while True:
-        line=reader.readlines()
-        if line:
-            obj = json.loads(line)
-            count += 1
-        else:
-            break
+for user in user_set:
+    user_count += 1
+    for suffix in suffix_list:
+        file_name = os.path.join(data_path_list, user + suffix)
+        if not os.path.exists(file_name):
+            continue
+        file_name = os.path.join(data_path_list, user + suffix)
+        with open(file_name, mode='r', encoding='utf8') as fp:
+            data = dict()
+            text_data = dict()
+            for line in fp.readlines():
+                try:
+                    for id, value in json.loads(line.strip()).items():
+                        if len(value) != 4:
+                            count += 1
+                except json.decoder.JSONDecodeError:
+                    count += 1
 print(count)
-# user_list = []
-# delete_list = []
-# with open('data/user_list/depression_user_list',mode='r') as fp:
-#     for line in fp.readlines():
-#         user, text = line.strip().split(' [info] ')
-#         if 'bipolar' in text:
-#             delete_list.append(user)
-#         else:
-#             user_list.append(line)
-
-# with open('data/user_list/depression_user_list_new', mode='w') as fp:
-#     for line in user_list:
-#         fp.write(line)
-# for user in delete_list:
-#     os.remove(os.path.join('data/state/depression', user))
-#     os.remove(os.path.join('data/reddit/depression', user))
