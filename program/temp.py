c976962ff75b2c544fa5b72eeb0414ec2238751e
guoxiaobo96 <guoxiaobo96@gmail.com>
2020-08-07 17:04:50 -0400

    update something

--- program/temp.py
+++ program/temp.py
@@ -7,27 +7,34 @@ data_path = './data/full_reddit'
 data_type_list = ['depression']
 user_list = []
 suffix_list = ['.before', '.after']
+anger_finish = True
+fear_finish = True
+fear_count = 0
+anger_count = 0
 # count = 0
 for type in data_type_list:
     data_folder = os.path.join(data_path, type)
     user_list_file = os.path.join(user_list_path, type) + '_user_list'
-    
-    with open(user_list_file, mode='r',encoding='utf8') as fp:
+
+    with open(user_list_file, mode='r', encoding='utf8') as fp:
         for line in fp.readlines():
             user = line.strip().split(' [info] ')[0]
             user_file = os.path.join(data_folder, user)
             for suffix in suffix_list:
-                if os.path.exists(user_file+suffix):
-                    user_list.append(user_file+suffix)
-    for user in user_list:
+                if os.path.exists(user_file + suffix):
+                    user_list.append(user_file + suffix) 
+    for id, user in enumerate(user_list):
         with open(user, mode='r') as fp:
             for line in fp.readlines():
-                for id, value in json.loads(line.strip()).items():
-                    text = value['text']
-                if text == '':
-                    count += 1
-    print(count)
-
+                try:
+                    for id, value in json.loads(line.strip()).items():
+                        if 'anger' not in value:
+                            anger_finish = False
+                        if 'fear' not in value:
+                            fear_finish = False
+                except:
+                    continue
+print('test')
 # file_path = './data/reddit/depression/ba6ee5a'
 # if os.path.exists(file_path):
 #     pass
